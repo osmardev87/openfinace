0ad929db3dd1180abd51bf676d77bc3ea1848904
Osmar Gomes <osmar.dev87@hotmail.com>
2026-08-26 12:50:45 +0000

    removido conta e renomeado versionamento do db

--- src/main/java/tech/gomesdev87/finace/conta/enums/FormaPagamento.java
+++
@@ -1,5 +0,0 @@
-package tech.gomesdev87.finace.conta.enums;
-
-public enum FormaPagamento {
-    PIX, BOLETO, DEPOSITO, DINHEIRO, OUTROS
-}
--- src/main/java/tech/gomesdev87/finace/conta/enums/TipoMovimento.java
+++
@@ -1,5 +0,0 @@
-package tech.gomesdev87.finace.conta.enums;
-
-public enum TipoMovimento {
-     ENTRADA, SAIDA
-}
